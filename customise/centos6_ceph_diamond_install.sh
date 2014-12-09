2fac4f9c72eae34d27665d9868c61651ccd5689f
kaihua.zhou <silegon@gmail.com>
2014-12-09 16:50:48 +0800

    fix

---
+++ customise/centos6_ceph_diamond_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cat /etc/resolve.conf
+
+sed -i "s/^nameserver\ .*/nameserver 114.114.114.114/g" /etc/resolv.conf
+pip install diamond
+
+
+rm -rf /etc/diamond/handlers/*
+rm -rf /etc/diamond/collectors/*
+
+wget --no-check-certificate -O /etc/diamond/handlers/GraphitePickleHandler.conf https://raw.githubusercontent.com/silegon/Diamond/master/customise/GraphitePickleHandler.conf
+wget --no-check-certificate -O /etc/diamond/collectors/CephCollector.conf https://raw.githubusercontent.com/silegon/Diamond/master/customise/CephCollector.conf
+
+wget --no-check-certificate -O /etc/diamond/diamond.conf.bak https://raw.githubusercontent.com/silegon/Diamond/master/customise/diamond.conf.bak
+sed "s/__hostname__/`hostname`/g" /etc/diamond/diamond.conf.bak > /etc/diamond/diamond.conf
+
+ntpdate cn.pool.ntp.org
+
+service diamond start
+
+sed -i "s/^nameserver\ .*/nameserver 127.0.0.1/g" /etc/resolv.conf
